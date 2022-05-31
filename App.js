f238bda39f46d7fbfbf430a46e93795545f9936c
Daniyal Alam <90774564+DaniyalAlam09@users.noreply.github.com>
2022-05-31 15:19:57 +0500

    Update

--- App.js
+++ App.js
@@ -1,98 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SectionList } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SectionList } from "react-native";
 
 const DATA = [
   {
-    category:"Main Dishes",
-    title:"Pizza",
-    key:0
+    category: "Main Dishes",
+    title: "Pizza",
+    key: 0,
   },
   {
-    category:"Main Dishes",
-    title:"Burger",
-    key:1
+    category: "Main Dishes",
+    title: "Burger",
+    key: 1,
   },
   {
-    category:"Main Dishes",
-    title:"Risotto",
-    key:2
+    category: "Main Dishes",
+    title: "Risotto",
+    key: 2,
   },
   {
-    category:"Sides",
-    title:"French Fries",
-    key:3
+    category: "Sides",
+    title: "French Fries",
+    key: 3,
   },
   {
-    category:"Sides",
-    title:"Onion Rings",
-    key:4
+    category: "Sides",
+    title: "Onion Rings",
+    key: 4,
   },
   {
-    category:"Sides",
-    title:"Fried Shrimps",
-    key:5
+    category: "Sides",
+    title: "Fried Shrimps",
+    key: 5,
   },
   {
-    category:"Drinks",
-    title:"Water",
-    key:6
+    category: "Drinks",
+    title: "Water",
+    key: 6,
   },
   {
-    category:"Drinks",
-    title:"Coke",
-    key:7
+    category: "Drinks",
+    title: "Coke",
+    key: 7,
   },
   {
-    category:"Drinks",
-    title:"Beer",
-    key:8
+    category: "Drinks",
+    title: "Beer",
+    key: 8,
   },
   {
-    category:"Desserts",
-    title:"Cheese Cake",
-    key:9
+    category: "Desserts",
+    title: "Cheese Cake",
+    key: 9,
   },
   {
-    category:"Desserts",
-    title:"Ice Cream",
-    key:10
+    category: "Desserts",
+    title: "Ice Cream",
+    key: 10,
   },
-]
+];
 
-var newData = []
-const uniqueValues = new Set(DATA.map(v => v.category));
-const uni = [...uniqueValues]
-uni.map((cat)=>{
-    var obj = {title:"",data:[]}
-    obj.title = cat
-    DATA.map((obj1)=>{
-      if(cat == obj1.category){
-        obj.data.push(obj1.title)
-      }
-    })
-    newData.push(obj)
-})
-console.log(newData)
-// let localeData = DATA.map((item) => {
-//   item.key = moment(item.category)
-//   return item
-// });
-
-// const section1 = () =>{
-//   var data,sec;
-//   for (let i=0; i<DATA.length; i++){
-//    data = DATA[i];
-//   sec = [];
-
-//   if (data.category == "Main Dishes"){
-//       sec.push(data)
-   
-//   }
-  
-// }
-
-
+var newData = [];
+const uniqueValues = new Set(DATA.map((v) => v.category));
+const uni = [...uniqueValues];
+uni.map((cat) => {
+  var obj = { title: "", data: [] };
+  obj.title = cat;
+  DATA.map((obj1) => {
+    if (cat == obj1.category) {
+      obj.data.push(obj1.title);
+    }
+  });
+  newData.push(obj);
+});
+console.log(newData);
 
 const Item = ({ title }) => (
   <View style={styles.item}>
@@ -100,20 +80,17 @@ const Item = ({ title }) => (
   </View>
 );
 
-
 export default function App() {
-
   return (
     <View style={styles.container}>
-      
       <SectionList
-      sections={newData}
-      keyExtractor={(item, index) => item + index}
-      renderItem={({ item }) => <Item title={item} />}
-      renderSectionHeader={({ section: { title } }) => (
-        <Text style={styles.header}>{title}</Text>
-      )}
-    />
+        sections={newData}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Item title={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
     </View>
   );
 }
@@ -122,18 +99,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: StatusBar.currentHeight,
-    marginHorizontal: 16
+    marginHorizontal: 16,
   },
   item: {
     backgroundColor: "#f9c2ff",
     padding: 20,
-    marginVertical: 8
+    marginVertical: 8,
   },
   header: {
     fontSize: 32,
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
   },
   title: {
-    fontSize: 24
-  }
+    fontSize: 24,
+  },
 });
